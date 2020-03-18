f5e086b98dce522f3e6c52a394c7bac0e3bc938b
Boido <filippo.boido@factoryautomation.info>
2020-03-18 08:55:19 +0100

    Projektdateien hinzufügen.

---
+++ LogReader/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LogReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LogReader starts now.");
+            Console.WriteLine("Press ENTER to quit.");
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine("");
+
+            using (FileStream stream = File.Open(@"C:\Users\fboid\Documents\ScopeTest\Log.csv", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    while (true) 
+                    {
+                        if (Console.KeyAvailable)
+                        {
+                            if (Console.ReadKey().Key == ConsoleKey.Enter)
+                                break;
+                        }
+                        
+                        if (!reader.EndOfStream)
+                            Console.Write(Convert.ToChar(reader.Read()));
+                        
+                    } 
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine("LogReader exits now.");
+            Thread.Sleep(1000);
+        }
+    }
+}
